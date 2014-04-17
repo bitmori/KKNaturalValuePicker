5145dfe5e5a92846a2cc6cc5dd95480a5d041afa
neonmori <neonmori@gmail.com>
2014-04-17 11:06:11 -0500

    Initial Commit

---
+++ NaturalValuePicker/KKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KKAppDelegate.h
+//  NaturalValuePicker
+//
+//  Created by Ke Yang on 4/17/14.
+//  Copyright (c) 2014 Pyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NaturalValuePicker/KKViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKViewController.h
+//  NaturalValuePicker
+//
+//  Created by Ke Yang on 4/17/14.
+//  Copyright (c) 2014 Pyrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKViewController : UIViewController
+
+@end
